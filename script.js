0ad46407b67030a75d961e189702ddc9ba71df51
Himavanth <himavanthnampally1356@gmail.com>
2022-04-12 12:56:15 +0530

    Changes made in js file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const body = document.querySelector("body");
 
-const apiUrl = "";
+const apiUrl = "http://localhost:8080";
 
 window.addEventListener("load",()=>{
     body.classList.add("visible");
@@ -26,4 +26,79 @@ document.querySelector(".noaccount").addEventListener("click", function(){
     alert("Please signup first");
     document.querySelector(".popup-signup").style.display = "flex";
     document.querySelector(".popup-login").style.display= "none";
+});
+
+const signIn = document.querySelector(".popup-login");
+
+signIn.addEventListener("submit",(event)=>{
+    event.preventDefault();
+   
+    const loginEmail = document.querySelector(".signin-email");
+    const loginPassword = document.querySelector(".signin-password");
+
+    const email = loginEmail.value;
+    const password = loginPassword.value;
+
+    fetch(`${apiUrl}/auth/signin`,{
+        method: "POST",
+        headers:{
+          "Content-Type": "application/json",
+        }, 
+        body: JSON.stringify({email,password}),
+       })
+       .then((res) => res.json())
+       .then((data) =>{
+           const {token} = data;
+          if(token){
+              localStorage.setItem("jwt", token);
+              location.href = "/Movie Page/movie.html";
+          }
+          else{
+              alert("SignIn Again");
+          }
+       })
+       .catch((err) =>{
+          alert("Error Signing In!! Re-try....");
+          console.log(err);
+       });
+       
+});
+
+const signUp= document.querySelector(".popup-signup");
+
+signUp.addEventListener("submit",(event)=>{
+    event.preventDefault();
+
+     const email = document.querySelector(".signup-email").value;
+     const name = document.querySelector(".signup-username").value;
+     const contact = document.querySelector(".signup-contact").value;
+     const password = document.querySelector(".signup-password").value;
+     const retypepassword = document.querySelector(".signup-retype-password").value;
+     if(password !== retypepassword){
+         alert("Passwords don't match");
+         return;
+     }
+    
+     fetch(`${apiUrl}/auth/signup`,{
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json",
+      }, 
+      body: JSON.stringify({name,email,password}),
+     })
+     .then((res) => res.json())
+     .then((data) =>{
+         const {token} = data;
+        if(token){
+            localStorage.setItem("jwt", token);
+            location.href="/Movie Page/movie.html";
+        }
+        else{
+            alert("SignUp Again");
+        }
+     })
+     .catch((err) =>{
+        alert("Error Signing Up!! Re-try....");
+        console.log(err);
+     });
 });
